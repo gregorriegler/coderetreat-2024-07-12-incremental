a4b5ddc3094f417f23c688ab50b4571cfb6eae8e
Gregor Riegler <rieglerg85@gmail.com>
2024-07-12 13:26:12 +0200

    Fix tests manually

--- observations.md
+++ observations.md
@@ -5,6 +5,12 @@ Without context, new implementation not related to existing one
 With context, implementation is correct but test cases fail
 We are still in the same session
 
+We change session:
+* Code != functionality, i.e. how do we reference the existing implementation in the prompt
+  how do we relate to it when we say it should be extended?
+* New session leads to new implementation
+
+We cannot prompt to force adjustment of existing tests
 
 TBD:
 * Effect of session (fresh vs ongoing)
--- test_incremental.py
+++ test_incremental.py
@@ -3,12 +3,15 @@ import pytest
 def replace_numbers(input_list):
     result = []
     previous_num = None
-    for num in input_list:
+    for i, num in enumerate(input_list):
         if num == 9:
             result.extend([10, 10])
         elif num == 2:
             if previous_num is not None:
                 result.extend([1] * previous_num)
+        elif num == 6:
+            if previous_num is not None and i + previous_num < len(input_list):
+                result.extend([3] * input_list[i + previous_num])
         else:
             result.append(num)
         previous_num = num
@@ -17,8 +20,8 @@ def replace_numbers(input_list):
 def test_replace_nines():
     # not working because this is 1st iteration
     # FAILS
-    input_list = [1, 3, 4, 5, 6, 7, 8, 9, 10]
-    expected_output = [1, 3, 4, 5, 6, 7, 8, 10, 10, 10]
+    input_list = [1, 3, 4, 5, 7, 8, 9, 10]
+    expected_output = [1, 3, 4, 5, 7, 8, 10, 10, 10]
     assert replace_numbers(input_list) == expected_output
 
 def test_replace_twos():
@@ -30,4 +33,9 @@ def test_replace_combined():
     # FAILS
     input_list = [1, 9, 2, 2, 3]
     expected_output = [1, 10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3]
+    assert replace_numbers(input_list) == expected_output
+
+def test_replace_sixes():
+    input_list = [1, 6, 3, 4, 5]
+    expected_output = [1, 3, 3, 3, 3, 4, 5]
     assert replace_numbers(input_list) == expected_output
